8aed9afa1dbe65e0ead8b2f3421e63199b4ce2e8
Esuong-Bassey <basseyesuong@gmail.com>
2021-12-21 12:26:47 -0800

    added first_java_project

---
+++ First_Java_Project/src/com/Graham/Main.java
@@ -0,0 +1,14 @@
+package com.Graham;
+
+public class Main {
+
+    public static void main(String[] args) {
+    System.out.println("Hello world. Its a Pleasure to be experience here");
+    System.out.println("Exploring java programming code");
+    System.out.println("Java is a strongly typed object oriented programming language");
+    System.out.println("java is roburst and stable");
+    System.out.println("java is portable and secure");
+    System.out.println("java is object oriented");
+    System.out.println("java is platform independent");
+    }
+}
